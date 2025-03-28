3e789b9ca24e96c05d7581d6843786d280c5e6e5
Damyy17 <90212650+Damyy17@users.noreply.github.com>
2025-03-28 18:35:26 +0200

    added new animations

--- src/Layout.tsx
+++ src/Layout.tsx
@@ -6,10 +6,34 @@ export default function Layout() {
 
     return(
         <> 
+            <svg style={{ position: "absolute", width: 0, height: 0 }}>
+                <defs>
+                    <filter id="grain" x="0" y="0" width="100%" height="100%">
+                        <feTurbulence
+                            type="fractalNoise"
+                            baseFrequency="0.7"
+                            numOctaves="3"
+                            stitchTiles="stitch"
+                            result="noise"
+                        />
+                        <feColorMatrix
+                            type="matrix"
+                            values="0 0 0 0 0
+                                    0 0 0 0 0
+                                    0 0 0 0 0
+                                    0 0 0 0.1 0"
+                            in="noise"
+                            result="grain"
+                        />
+                        <feComposite in="SourceGraphic" in2="grain" operator="in" />
+                    </filter>
+                </defs>
+            </svg>
+
             <div className="gradient-bg">
                 <div className="blobs-container">
                     <img src="./src/assets/images/blob-combo.png" className="blob1"  />
-                    <img src="./src/assets/images/blob-combo.png" className="blob2"  />
+                    <img src="./src/assets/images/blob-combo2.png" className="blob2"  />
                     <div className="glass-overlay"></div>
                 </div>
             </div>
--- src/components/navbar.tsx
+++ src/components/navbar.tsx
@@ -1,6 +1,5 @@
 import { NavLink } from "react-router";
 import "../assets/styles/navbar.sass"
-import gotoIcon from '../assets/icons/go-tosvg.svg'
 
 export default function Navbar() {
 
@@ -16,8 +15,11 @@ export default function Navbar() {
             </div>
             <div className="get-in-touch">
                 <div>Get In Touch</div>
-                {/* <div>svg</div> */}
-                <img src={gotoIcon} className="goto" alt="" />
+                <svg width="24" height="24" viewBox="0 0 24 24" fill="none" xmlns="http://www.w3.org/2000/svg">
+                    <path d="M7 17L17 7" stroke="#F5F5F5" stroke-width="2" stroke-linecap="round" stroke-linejoin="round"/>
+                    <path d="M7 7H17V17" stroke="#F5F5F5" stroke-width="2" stroke-linecap="round" stroke-linejoin="round"/>
+                </svg>
+
             </div>
         </div>
     </nav>
